2833427c242947d9ec0fc51db757ce4ff92ac582
Parth Zile <parthzile01@gmail.com>
2024-01-02 10:34:55 +0530

    Completed the Exercise

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,17 @@
 // Complete this component
 
+const Image = ({data}) => {
 
-
+    return(
+        <div>
+        {data.map(function(element){
+            return <img src={element.img} alt="" key={element.id}/>
+        })
+    }
+        </div>
+    
+    )
+}
 
 
 
